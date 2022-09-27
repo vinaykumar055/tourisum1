ea7bc97f2a8e3ba70df07be9e8ee14a3042bc43b
hp <hp@DESKTOP-D9BKGLB>
2022-09-27 15:51:55 +0530

    new commit

--- src/test/java/GithubPackage/CreateTest.java
+++ src/test/java/GithubPackage/CreateTest.java
@@ -4,9 +4,13 @@ import org.testng.annotations.Test;
 
 public class CreateTest {
 	@Test
-	public void create() {
+	public void create() {	
 		
 		
+		System.out.println("created");
+	}
+	@Test
+	public void createnewTest() {	
 		
 		
 		System.out.println("created");
